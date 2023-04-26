f7347e8d87483f744c3e48d77dbe9485f757523a
Mathieu Blanchette <m.blanchette@agendrix.com>
2023-04-26 12:31:54 -0400

    Create get-ios-app-version action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import * as core from "@actions/core";
-import { wait } from "./wait";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput("milliseconds");
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const projectYmlPath: string = core.getInput("project-yml-path");
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    if (!fs.existsSync(projectYmlPath)) {
+      core.setFailed(`The file path for project.yml file does not exist or is not found: ${projectYmlPath}`);
+      process.exit(1);
+    }
 
-    core.setOutput("time", new Date().toTimeString());
+    core.debug(`Running task with ${projectYmlPath}`);
+
+    const projectYmlFile = fs.readFileSync(projectYmlPath, { encoding: "utf8" });
+    const projectYmlFileContent: any = yaml.load(projectYmlFile);
+    const bundleShortVersionString: string = projectYmlFileContent.settings.base.MARKETING_VERSION;
+
+    core.setOutput("app-version", bundleShortVersionString);
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(`Action failed with error ${error}`);
   }
 }
 
